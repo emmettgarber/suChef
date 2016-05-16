d505e75d0523f7badbe1139218153a955b876021
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-05-15 19:27:58 -0500

    User profile

--- app/assets/javascripts/components/events/EditProfile.js.jsx
+++ app/assets/javascripts/components/events/EditProfile.js.jsx
@@ -2,7 +2,7 @@ var EditProfile = React.createClass({
   getInitialState: function() {
     return {
       checked: this.props.checked || false,
-      profile: {}
+      profile: ""
     };
   },
   componentWillMount: function() {
@@ -30,15 +30,22 @@ var EditProfile = React.createClass({
       }
     }.bind(this));
   },
+
   render: function() {
     var cuisineObject = {french: true, mexican: false, italian: false, lithuanian: true};
-    var profile = this.state.profile.verifiedAwesome;
-    var formattedProfile = {verifiedAwesome}
-    console.log(formattedProfile);
+    var profile = this.state.profile.verifiedAwesome
+    if (this.state.profile) {
+      console.log(this.state.profile.verifiedAwesome[0]);
+    }
     return (
       <div>
         <h3>Verified awesome in the following cuisines</h3>
+        <ul>
+         {this.state.profile.map(function(index, listValue){
 
+           return <li>{listValue.verifiedAwesome[index]}</li>;
+         })}
+       </ul>
       </div>);
   }
 })
