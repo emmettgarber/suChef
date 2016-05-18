4731d441608e5f6ea1654be5d8eab6170abd5095
jonnorstrom <jon.norstrom@gmail.com>
2016-05-18 15:19:04 -0500

    add emoji back in

--- app/assets/javascripts/components/events/EventGrid.js.jsx
+++ app/assets/javascripts/components/events/EventGrid.js.jsx
@@ -1,13 +1,23 @@
 var EventGrid = React.createClass({
+  getIcon: function(viewing) {
+    if (viewing.instructor_id == null && viewing.apprentice_id != null) {
+      return <Sad/>
+    } else if (viewing.instructor_id != null && viewing.apprentice_id == null) {
+      return <Sad/>
+    } else {
+      return <Happy/>
+    }
+  },
+
   getClasses: function(array) {
     return array.map(function(viewing, i) {
       var time = moment(viewing.starttime).format("dddd MMMM Do, [at] h:mm a").toString();
       var holder = viewing
       return (
         <div key={i} className="fillers">
-        <div className="body">
-          <p>{viewing.user_type}</p>
-        </div>
+          <div className="body">
+            <p>{viewing.user_type}{this.getIcon(viewing)}</p>
+          </div>
           <div className="body">
             <p>{viewing.dish}</p>
           </div>
@@ -15,7 +25,7 @@ var EventGrid = React.createClass({
             <p>{time}</p>
           </div>
           <div className="body">
-            <HangoutButton 
+            <HangoutButton
               profile= {this.props.profile} room= {holder}
             />
           </div>
