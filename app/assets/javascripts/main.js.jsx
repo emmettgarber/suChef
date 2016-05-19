d6dfb4df1cabb1d72dc69086d487c279649f6b1c
Emmett <emmett.garber@gmail.com>
2016-05-19 10:21:27 -0500

    Using jquery to add script tag

--- app/assets/javascripts/main.js.jsx
+++ app/assets/javascripts/main.js.jsx
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   ReactDOM.render(<App />, document.querySelector('#app-container'));
   // window.mySwipe = Swipe(document.getElementById('slider'));
+  $('body').append(<script src="https://apis.google.com/js/platform.js" async defer></script>)
 });
